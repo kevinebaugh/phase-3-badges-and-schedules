253aaadae2cfc733a0179f06b74867fd0e1726ff
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-01-18 14:14:44 -0500

    commit tree 88a871dcb6876d35aa1029fdfc2309a951307098

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map{ |name| badge_maker(name) }
+end
+
+def assign_rooms(names)
+  assignments = []
+  names.each_with_index do |name, index|
+    assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end
+  assignments
+end
+
+def printer(names)
+  badges = batch_badge_creator(names)
+  badges.each do |badge|
+    puts badge
+  end
+
+  assignments = assign_rooms(names)
+  assignments.each do |assignment|
+    puts assignment
+  end
+end
